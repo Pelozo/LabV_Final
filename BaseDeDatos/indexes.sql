9827c1565dfd430fa411437f17b028301f25c4aa
Martin Esteban Caminero <56420044+martin6446@users.noreply.github.com>
2021-06-17 19:12:08 -0300

    Update indexes.sql

--- BaseDeDatos/indexes.sql
+++ BaseDeDatos/indexes.sql
@@ -24,3 +24,27 @@ WHERE c.id = 1 AND m.date BETWEEN "2021-06-17 16:32:08" AND "2021-07-23 16:32:08
 
 -- verificamos que los indices existan
 SHOW INDEX FROM measurements
+
+
+-- devuelve las mediciones por usuario y fecha
+DROP PROCEDURE IF EXISTS stp_get_measurements_by_client_between_dates
+DELIMITER //
+CREATE PROCEDURE stp_get_measurements_by_client_between_dates(p_client_id INT, p_from DATETIME, p_to DATETIME)
+BEGIN
+
+-- conseguimos todos los datos necesarios 
+SELECT c.*,m.*,mt.*
+FROM measurements m
+JOIN residences r
+ON r.id = m.residence_id
+JOIN clients c
+ON r.client_id = c.id
+JOIN meters mt
+ON mt.id = r.meter_id
+WHERE c.id = p_client_id AND m.date BETWEEN p_from AND p_to ;
+
+END;
+
+DELIMITER ;
+
+CALL stp_get_measurements_by_client_between_dates(1,"2020-06-17 16:32:08","2022-06-17 08:44:26");
